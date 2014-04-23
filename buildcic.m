9e60b1f216ae675d92b54c55d917a3890b9bf1a8
Daniel Tavares <daniel.tavares@lnls.br>
2014-04-23 20:49:56 -0300

    buildcic.m: add new method for calculating decimated CIC

--- buildcic.m
+++ buildcic.m
@@ -1,8 +1,32 @@
-function [num, den] = buildcic(decfactor, nstages, ndelays)
+function [num, den] = buildcic(decfactor, nstages, ndelays, method, density)
+
+if nargin < 4 || isempty(method)
+    method = 'freqz';
+end
+if nargin < 5 || isempty(density)
+    density = 10;
+end
 
 cic_coeff = tf(mfilt.cicdecim(decfactor, ndelays, nstages));
-num = interp1(1:length(cic_coeff), cic_coeff, linspace(1, length(cic_coeff), nstages*ndelays+1));
-num((ceil(length(num)/2)+1):end) = num((floor(length(num)/2)):-1:1);
-num = num/sum(num);
+cic_coeff = cic_coeff/sum(cic_coeff);
+n = length(cic_coeff);
+ndecim = nstages*ndelays;
 
-den = [1 zeros(1,length(num)-1)];
+if strcmpi(method, 'freqz')
+    [h,w] = freqz(cic_coeff, [1 zeros(1,n-1)], density*ceil(n/decfactor)*decfactor);
+
+    h = h(1:n/decfactor*density-1);
+    w = linspace(0,pi,density*n/decfactor);
+    w = w(1:end-1);
+
+    num =invfreqz(h,w,ndecim,0);
+    num = num/sum(num);
+    den = [1 zeros(1,ndecim-1)];
+
+elseif strcmpi(method, 'decim')
+    num = interp1(1:length(cic_coeff), cic_coeff, linspace(1, length(cic_coeff), ndecim+1-rem(ndecim,2)));
+    num((ceil((length(num)-1)/2)+1):end) = num((floor((length(num)-1)/2)+1):-1:1);
+    num = num/sum(num);
+    den = [1 zeros(1,length(num)-1)];
+
+end
