332f8dc22062b0063954f324d7704f7df4d7918f
Daniel Tavares <daniel.tavares@lnls.br>
2014-05-09 11:33:20 -0300

    create a new function, multiplot, in order to replace plot_spectrum; make corresponding modifications on plot_spectrum_bpm to allow the use of the multiplot function

---
+++ uvx/multiplot.m
@@ -0,0 +1,80 @@
+function haxis = multiplot(x, yz, xrange, yzrange, color, logscale, xlabeltext, yzlabeltext, yznames, dataname, haxis, yy, yyrange, yylabeltext)
+
+% Check input arguments
+ny = size(yz, ndims(yz));
+
+if (nargin < 3) || isempty(xrange)
+    xrange = [x(1) x(end)];
+end
+
+if (nargin < 5) || isempty(color)
+    color = 'b';
+end
+
+if (nargin < 6) || isempty(logscale)
+    logscale = false;
+end
+
+if (nargin < 4) || isempty(yzrange)
+    if logscale
+        yzrange = [max(min(min(yz)), 1e-6) max(max(yz))];
+    else
+        yzrange = [min(min(yz)) max(max(yz))];
+    end
+end
+
+if (nargin < 7) || isempty(xlabeltext)
+    xlabeltext = '';
+end
+
+if (nargin < 8) || isempty(yzlabeltext)
+    yzlabeltext = 'a.u.';
+end
+
+if (nargin < 9)
+    yznames = [];
+end
+
+if (nargin < 10)
+    dataname = [];
+end
+
+% Plot
+for i = 1:ny
+    if (nargin < 11) || isempty(haxis)
+        figure;
+        haxis(i) = gca;
+    else
+        axes(haxis(i));
+    end
+    switch ndims(yz)
+        case 2
+            hnew = plot(x, yz(:,i), 'Color', color);
+        case 3
+            hnew = surf(x, yy, yz(:,:,i)', 'EdgeColor', 'none', 'LineStyle', 'none', 'FaceLighting', 'phong');
+            view(0,90);
+    end
+    hold on;
+    grid on;
+    if logscale
+        set(haxis(i), 'YScale', 'log');
+    else
+        set(haxis(i), 'YScale', 'linear');
+    end
+    axis([xrange yzrange])
+    xlabel(xlabeltext, 'FontSize', 14, 'FontWeight', 'bold');    
+    ylabel(yzlabeltext, 'FontSize', 14, 'FontWeight', 'bold');
+    set(haxis(i), 'FontSize', 14);
+    if ~isempty(yz)
+        [~,~,outh,outm] = legend;
+        n = length(outm);
+        outm{n+1} = '';
+        if ~isempty(yznames)
+            outm{n+1} = yznames{i};
+        end
+        if ~isempty(dataname)
+            outm{n+1} = [outm{n+1} ' - ' dataname];
+        end
+        legend([outh;hnew], outm, 'FontSize', 10);
+    end
+end
--- uvx/plot_spectrum_bpm.m
+++ uvx/plot_spectrum_bpm.m
@@ -1,5 +1,5 @@
-function haxis_out = plot_spectrum_bpm(data, graph_type, selected_bpms, window, color, log, dataset_name, haxis)
-% haxis_out = fofb_fa_plot_spectrum_bpm(data, graph_type, selected_bpms, window, color, log, dataset_name, haxis)
+function haxis_out = plot_spectrum_bpm(data, graph_type, selected_bpms, window, color, log, dataset_name, haxis, spectra_range, freq_range)
+% haxis_out = fofb_fa_plot_spectrum_bpm(data, graph_type, selected_bpms, window, color, log, dataset_name, haxis, spectra_range, freq_range)
 
 time = double(data.time-data.time(1))/1e9; % seconds, relative time
 Fs = 1/mean(double(diff(time)));
@@ -22,7 +22,7 @@ selected_signals = 1e3*data.bpm_readings(:, selected_readings);
 selected_bpm_names = data.bpm_names(selected_readings);
 
 if (nargin < 2)
-    graph_type = 'fft';
+    graph_type = 'dft';
 end
 
 if (nargin < 4)
@@ -54,9 +54,36 @@ if (nargin < 8) || isempty(haxis)
         set(fig, 'Name', [selected_bpm_names{2*i-1} ' - ' selected_bpm_names{2*i}], 'NumberTitle', 'off');
         set(fig, 'WindowStyle', 'docked');
     end
+else
+    haxis = [];
+end
+
+if (nargin < 9)
+    spectra_range = [];
+end
+
+if (nargin < 10)
+    freq_range = [];
 end
 
 % ===========
 % Plot graphs
 % ===========
-haxis_out = plot_spectrum(selected_signals, Fs, graph_type, [], [], window, color, log, selected_bpm_names, 'um', dataset_name, haxis);
+if (nargin < 1) || isempty(selected_signals)
+    spectra = [];
+    freq = [];
+    spectrum_ylabel = [];
+elseif strcmpi(graph_type, 'dft')
+    [spectra, freq] = fourierseries(selected_signals, Fs, window);
+    spectrum_ylabel = 'Amplitude (um)';
+elseif strcmpi(graph_type, 'psd')
+    [spectra, freq] = psdrms(selected_signals, Fs, 0, Inf, window, [], [], graph_type);
+    spectrum_ylabel = 'PSD (um/sqrt(Hz))';
+elseif strcmpi(graph_type, 'rms')
+    [spectra, freq] = psdrms(selected_signals, Fs, 0, Inf, window, [], [], graph_type);
+    spectrum_ylabel = 'Integrated RMS (um)';
+else
+    error('Invalid value for ''graph_type'' parameter.');
+end
+
+haxis_out = multiplot(freq, spectra, freq_range, spectra_range, color, log, 'Frequency (Hz)', spectrum_ylabel, selected_bpm_names, dataset_name, haxis);
