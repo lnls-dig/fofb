3d9f579aafed0924598b60ac4d82545092757020
Daniel Tavares <daniel.tavares@lnls.br>
2014-05-13 10:54:09 -0300

    control/: several modifications on Simulink model and Matlab functions to allow simulation of SOFB and FOFB simultaneously

--- control/repss.m
+++ control/repss.m
@@ -1,16 +1,31 @@
-function [a,b,c,d] = repss(a,b,c,d,n)
+function [A,B,C,D] = repss(A,B,C,D,n)
 %REPSS Replicate state-space model N times.
 %
-%   [a,b,c,d] = repss(a,b,c,d,n)
+%   [A,B,C,D] = repss(A,B,C,D,n)
 
-[acell{1:n}] = deal(a);
-a = blkdiag(acell{:});
+if ~iscell(A) && ~iscell(B) && ~iscell(C) && ~iscell(D)
+    if nargin >= 5 && isscalar(n) && n > 0
+        [Acell{1:n}] = deal(A);
+        [Bcell{1:n}] = deal(B);
+        [Ccell{1:n}] = deal(C);
+        [Dcell{1:n}] = deal(D);
+    else
+        error('Must specify how many times matrices should be replicated.')
+    end
+elseif iscell(A) && iscell(B) && iscell(C) && iscell(D)
+    if nargin < 5
+        Acell = A;
+        Bcell = B;
+        Ccell = C;
+        Dcell = D;
+    else
+        error('Cannot replicate matrices.')
+    end
+else
+    error('A,B,C,D inputs must be all matrices or all cell of matrices.');
+end
 
-[bcell{1:n}] = deal(b);
-b = blkdiag(bcell{:});
-
-[ccell{1:n}] = deal(c);
-c = blkdiag(ccell{:});
-
-[dcell{1:n}] = deal(d);
-d = blkdiag(dcell{:});
+A = blkdiag(Acell{:});
+B = blkdiag(Bcell{:});
+C = blkdiag(Ccell{:});
+D = blkdiag(Dcell{:});
--- control/simfofb_sirius.m
+++ control/simfofb_sirius.m
@@ -10,20 +10,16 @@ Md = Mdq(:,:,3);
 nbpm = size(M,1);
 ncorr = size(M,2);
 ndist = size(Md,2);
-param.RespMat = M;
-param.CorrMat = pinv(param.RespMat);
-param.DistMat = Md;
+param.respmat = M;
+corrmat = pinv(param.respmat);
+param.distmat = Md;
 
 % BPM (FOFB sensor)
 bpm_cic_decfactor = 1015;
 bpm_cic_nstages = 2;
-bpm_cic_ndelays = 3;
+bpm_cic_ndelays = 1;
 [param.bpm.num, param.bpm.den] = buildcic(bpm_cic_decfactor, bpm_cic_nstages, bpm_cic_ndelays);
 param.bpm.Ts = 10e-6;
-
-% DCCT (Power supply sensor)
-param.dcct.num = 1;
-param.dcct.den = 1;
 
 % Power supply controller
 param.psctrl.num = 1;
@@ -32,13 +28,16 @@ param.psctrl.Ts = 10e-6;
 param.psctrl.manual = true;
 
 % Power supply output filter
-param.psfilter.num = 1;
-param.psfilter.den = 1;
+param.corrsofbpsfilter.num = 1;
+param.corrsofbpsfilter.den = 1;
 
-% Corrector magnet
+% Corrector magnet current
 corrmagnet_L = 20e-3;
 corrmagnet_R = 1;
 [param.corrmagnet.num, param.corrmagnet.den] = build1order(corrmagnet_L/corrmagnet_R);
+
+% Corrector magnet current/magnetic field gain
+param.corrmagnetgain = ones(ncorr,1);
 
 % Vacuum chamber
 vac_radius = 12e-3;
@@ -48,21 +47,40 @@ mu0 = 4*pi*1e-7;
 [param.vacchamb.num, param.vacchamb.den] = build1order(0.5*mu0*vac_conductivity*vac_radius*vac_thickness);
 
 % FOFB controller
-[a,b,c,d] = tf2ss(0.0001*[1 0], [1 -1]);
+[a,b,c,d] = tf2ss(0.001*[1 0], [1 -1]);
 [param.fofbctrl.a, b, param.fofbctrl.c, d] = repss(a,b,c,d,ncorr);
-param.fofbctrl.b = b*param.CorrMat;
-param.fofbctrl.d = d*param.CorrMat;
+param.fofbctrl.b = b*corrmat;
+param.fofbctrl.d = d*corrmat;
 param.fofbctrl.Ts = 10e-6;
-param.fofbctrl.sensdelay = 10e-6;
-param.fofbctrl.actdelay = 10e-6;
+
+% SOFB controller
+param.sofbctrl.a = [];
+param.sofbctrl.b = [];
+param.sofbctrl.c = [];
+param.sofbctrl.d = ones(1,nbpm);
+param.sofbctrl.Ts = 1;
+
+% Communication network delays
+param.netdelay.bpm = 10e-6;
+param.netdelay.corrfofb = 10e-6;
+param.netdelay.corrsofb = 10e-6;
+
+
+%%%%%%%%%%%%
+ncorrsofb = 1;
+param.corrselectsofb = 1;
+param.ncorr = 1;
+param.corrordering = 1+(1:ncorr);
+
 
 % Build simulation vectors
-t = (0:1e-6:1)';
+t = (0:1e-6:0.05)';
 npts = length(t);
 param.simvectors.t = t;
-param.simvectors.dist = 0.001*[zeros(200000,ndist); ones(npts-200000,ndist)];
+param.simvectors.reference_orbit = zeros(npts,nbpm);
+param.simvectors.orbit_distortion = 0.001*[zeros(2000,ndist); ones(npts-2000,ndist)];
 param.simvectors.bpm_noise = 140e-9*randn(npts,nbpm);
-param.simvectors.dcct_noise = 3e-4*randn(npts,ncorr);
-param.simvectors.psoutput_noise = 3e-4*randn(npts,ncorr);
+param.simvectors.sloworbcorr_current_noise = 3e-4*randn(npts,ncorrsofb);
+param.simvectors.sloworbcorr_power_noise = 3e-4*randn(npts,ncorrsofb);
 
 simout = simfofb(param);
