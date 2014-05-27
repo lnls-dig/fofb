019c3110d3e184f1d15a8b6d4d02498891064be6
Daniel Tavares <daniel.tavares@lnls.br>
2014-05-27 13:03:18 -0300

    uvx/faload.m: load new file format (by data blocks)

--- uvx/faload.m
+++ uvx/faload.m
@@ -6,39 +6,30 @@ function fadata = faload(filenames)
 if ischar(filenames)
     try
         fileid = fopen(filenames);
-        period = fread(fileid,1,'uint32','l');
-        
-        header_bpm = fgetl(fileid);
-        if ~isempty(header_bpm)
-            header_bpm = textscan(header_bpm, '%s', 'delimiter', '\t');
-            header_bpm = header_bpm{1};
-        else
-            header_bpm = {};
-        end        
+        period = fread(fileid, 1, 'uint32', 'l');
+
+        header_bpm = readheader(fileid);
         length_bpm = length(header_bpm);
         
-        header_ps = fgetl(fileid);
-        if ~isempty(header_ps)
-            header_ps = textscan(header_ps, '%s', 'delimiter', '\t');
-            header_ps = header_ps{1};
-        else
-            header_ps = {};
-        end
+        header_ps = readheader(fileid);
         length_ps = length(header_ps);
-        
-        nvars = fread(fileid,1, 'uint32', 'l');
 
-        fileinfo = dir(filenames);
-        nrows = (fileinfo.bytes-ftell(fileid))/4/(nvars+2);
-        
-        data = fread(fileid, nrows*(nvars+2), 'single=>single', 'l');
-        data = reshape(data, nvars+2, nrows)';
+        data = [];
+        while true
+            nrows = fread(fileid, 1, 'uint32=>uint32', 'l');
+            ncols = fread(fileid, 1, 'uint32=>uint32', 'l');
+            if isempty(ncols) || isempty(nrows)
+                break;
+            end
+            subdata = fread(fileid, ncols*nrows, 'single=>single', 'l');
+            data = [data; reshape(subdata, ncols, nrows)'];
+        end
 
         time_hi = typecast(data(:,end), 'uint32');
         time_lo = typecast(data(:,end-1), 'uint32');
         time = bitor(bitshift(uint64(time_hi), 32), uint64(time_lo));
-                
-        data = data(:, 1:end-2);
+
+        data = data(:,1:end-2);
 
     catch err
         fclose(fileid);
@@ -67,17 +58,17 @@ elseif iscell(filenames)
            'period', []);
 
     for i=1:length(filenames)
-        subfadata = faload(filenames{i});
+        sub_fadata = faload(filenames{i});
         
-        fadata.time         = [fadata.time;         subfadata.time];
-        fadata.bpm_readings = [fadata.bpm_readings; subfadata.bpm_readings];
-        fadata.ps_readings  = [fadata.ps_readings;  subfadata.ps_readings];
-        fadata.ps_setpoints = [fadata.ps_setpoints; subfadata.ps_setpoints];
+        fadata.time         = [fadata.time;         sub_fadata.time];
+        fadata.bpm_readings = [fadata.bpm_readings; sub_fadata.bpm_readings];
+        fadata.ps_readings  = [fadata.ps_readings;  sub_fadata.ps_readings];
+        fadata.ps_setpoints = [fadata.ps_setpoints; sub_fadata.ps_setpoints];
         
-        fadata.bpm_names = compare(fadata.bpm_names, subfadata.bpm_names);
-        fadata.ps_names = compare(fadata.ps_names, subfadata.ps_names);
-        fadata.ps_setpoints_names = compare(fadata.ps_setpoints_names, subfadata.ps_setpoints_names);
-        fadata.period = compare(fadata.period, subfadata.period);
+        fadata.bpm_names = compare(fadata.bpm_names, sub_fadata.bpm_names);
+        fadata.ps_names = compare(fadata.ps_names, sub_fadata.ps_names);
+        fadata.ps_setpoints_names = compare(fadata.ps_setpoints_names, sub_fadata.ps_setpoints_names);
+        fadata.period = compare(fadata.period, sub_fadata.period);
     end
 end
 
@@ -86,3 +77,13 @@ function new = compare(current, new)
 if ~(isempty(current) || isequal(current, new))
     error('Corresponding headers and constants across different files shall be identical.');
 end
+
+function header = readheader(fileid)
+
+header = fgetl(fileid);
+if ~isempty(header)
+    header = textscan(header, '%s', 'delimiter', '\t');
+    header = header{1};
+else
+    header = {};
+end   
