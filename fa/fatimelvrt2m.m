2b3e7efc80c8f1ea0d7bc88a2cd0199d9ab7b9db
Daniel Tavares <daniel.tavares@lnls.br>
2014-07-09 12:43:35 -0300

    fa/fatimelvrt2m.m: better implementation based on differences between consecutive items of the timestamp array

--- fa/fatimelvrt2m.m
+++ fa/fatimelvrt2m.m
@@ -1,3 +1,3 @@
 function time_matlab = fatimelvrt2m(time_labviewrt)
 
-time_matlab = (double(bitsrl(time_labviewrt, 19))*2^19/1e6)/1000/60/60/24 + datenum('1904/1/1') - java.util.Date().getTimezoneOffset()/60/24;
+time_matlab = (cumsum([0; double(diff(time_labviewrt))]) + double(time_labviewrt(1)))/1e9/60/60/24 + datenum('1904/1/1') - java.util.Date().getTimezoneOffset()/60/24;
