73845c7fd6f41693a039c706ae586e6f05b2555a
Daniel Tavares <daniel.tavares@lnls.br>
2014-07-09 12:45:48 -0300

    modelid/idcorrps.m: absorb poles at 0 as LTI models time delay

--- modelid/idcorrps.m
+++ modelid/idcorrps.m
@@ -1,4 +1,4 @@
-function [sysset, fit, noisesysset] = idcorrps(indata, outdata, names, idmethod, order, percent, nk)
+function [sysd, fit] = idcorrps(indata, outdata, names, idmethod, order, percent, nk, Ts)
 
 if nargin < 6 || isempty(percent)
     percent = 80;
@@ -6,6 +6,10 @@ end
 
 if nargin < 7
     nk = [];
+end
+
+if nargin < 8
+    Ts = -1;
 end
 
 ncorr = size(indata,2);
@@ -16,18 +20,25 @@ idout = outdata(1:npts_ident,:);
 valin = indata(npts_ident+1:end,:);
 valout = outdata(npts_ident+1:end,:);
 
-sysset = ss([],[],[],[]);
-noisesysset = ss([],[],[],[]);
+sysd = zpk([],[],1,-1);
 fit = zeros(ncorr,1);
 
 for i=1:size(indata,2)
     if isempty(nk)
         nk_ = delayest(iddata(outdata(:,i), indata(:,i)));
+    else
+        nk_ = nk;
     end
     sys = idmethod(iddata(idout(:,i), idin(:,i)), [order nk_]);
     syszpk = zpk(sys);
-    sysset(i,i) = syszpk(1,1);
-    noisesysset(i,i) = syszpk(1,2);
+    syszpk = syszpk(1,1);
+    syszpk.Ts = Ts;
+    syszpk = syszpk/dcgain(syszpk);
+    sysd(i,i) = syszpk;
+    dly = length(find(syszpk.p{1} == 0));
+    syszpk.p = {syszpk.p{1}((syszpk.p{1} ~= 0))};
+    syszpk = d2c(zpk(syszpk));
+    syszpk.inputdelay = Ts*dly;
     [~, fit(i)] = compare(iddata(valout(:,i), valin(:,i)), sys);
     fprintf('%s: %0.1f%% \n', names{i}, fit(i));
 end
