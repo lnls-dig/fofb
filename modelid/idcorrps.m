829aec3a5006264ff4cec1bd9df24493bc359f40
Daniel Tavares (Unicamp) <danielot@gmail.com>
2014-07-27 10:16:39 -0300

    modelid/idcorrps.m: now idcorrps expects an iddata object instead of a data arrays

--- modelid/idcorrps.m
+++ modelid/idcorrps.m
@@ -1,35 +1,36 @@
-function [sysd, fit] = idcorrps(indata, outdata, names, idmethod, order, percent, nk, Ts)
+function [sysd, fit, sys_array] = idcorrps(data, idmethod, order, idvalfactor, nk, Ts)
 
-if nargin < 6 || isempty(percent)
-    percent = 80;
+if nargin < 4 || isempty(idvalfactor)
+    idvalfactor = 2/3;
 end
 
-if nargin < 7
+if nargin < 5
     nk = [];
 end
 
-if nargin < 8
+if nargin < 6
     Ts = -1;
 end
 
-ncorr = size(indata,2);
-npts_ident = floor(size(indata,1)*percent/100);
-
-idin = indata(1:npts_ident,:);
-idout = outdata(1:npts_ident,:);
-valin = indata(npts_ident+1:end,:);
-valout = outdata(npts_ident+1:end,:);
+ncorr = length(data);
 
 sysd = zpk([],[],1,-1);
 fit = zeros(ncorr,1);
 
-for i=1:size(indata,2)
+for i=1:ncorr
+    
+    npts_ident = floor(size(data{i},1)*idvalfactor);
+
+    data_ident = data{i}(1:npts_ident);
+    data_val = data_ident; data{i}(npts_ident+1:end);
+    
     if isempty(nk)
-        nk_ = delayest(iddata(outdata(:,i), indata(:,i)));
+        nk_ = delayest(data_ident);
     else
         nk_ = nk;
     end
-    sys = idmethod(iddata(idout(:,i), idin(:,i)), [order nk_]);
+    sys = idmethod(data_ident, [order nk_]);
+    sys_array{i} = sys;
     syszpk = zpk(sys);
     syszpk = syszpk(1,1);
     syszpk.Ts = Ts;
@@ -37,8 +38,6 @@ for i=1:size(indata,2)
     sysd(i,i) = syszpk;
     dly = length(find(syszpk.p{1} == 0));
     syszpk.p = {syszpk.p{1}((syszpk.p{1} ~= 0))};
-    syszpk = d2c(zpk(syszpk));
-    syszpk.inputdelay = Ts*dly;
-    [~, fit(i)] = compare(iddata(valout(:,i), valin(:,i)), sys);
-    fprintf('%s: %0.1f%% \n', names{i}, fit(i));
+    [~, fit(i)] = compare(data_val, sys);
+    fprintf('%s: %0.1f%% \n', data_ident.OutputName{1}, fit(i));
 end
