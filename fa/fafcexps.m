da82e4cde902bb684c2d3dcced4bc4982b81ed59
Daniel Tavares (Unicamp) <danielot@gmail.com>
2014-07-27 10:05:28 -0300

    fa/fafcexps.m, fa/fa2iddata.m: store identification data on iddata objects

--- fa/fafcexps.m
+++ fa/fafcexps.m
@@ -1,8 +1,13 @@
-function fadata_array = fafcexps(fadata, start, npts_exp, npts_interval)
+function [fadata_array, corr_iddata, bpm_iddata] = fafcexps(fadata, start, npts_exp, npts_interval)
 
-fadata_array{1} = facutdata(fadata, start-npts_exp + (0:npts_exp-1),[],[]);
-fadata_array{2} = facutdata(fadata, start + (0:npts_exp-1),[],[]);
+i = 1;
 
-for i=1:42
-    fadata_array{i+2} = facutdata(fadata, i*(npts_exp +npts_interval) + start + (0:npts_exp-1),[],i);
+fadata_array{i} = facutdata(fadata, start + (0:npts_exp-1),[],[]);
+[corr_iddata{i}, bpm_iddata{i}] = fa2iddata(fadata_array{i});
+i=i+1;
+
+for j=1:42
+    fadata_array{i} = facutdata(fadata, j*(npts_exp +npts_interval) + start + (0:npts_exp-1),[],j);
+    [corr_iddata{i}, bpm_iddata{i}] = fa2iddata(fadata_array{i});
+    i=i+1;
 end
