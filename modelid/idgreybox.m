1504c4420e80f620a3e5776bbd9c79fa048174f0
Daniel Tavares (Unicamp) <danielot@gmail.com>
2014-08-01 20:12:01 -0300

    modelid/idgreybox.m

--- modelid/idgreybox.m
+++ modelid/idgreybox.m
@@ -14,14 +14,16 @@
 %     syscorrarray{corr} = m3(1,1);
 % end
 
-bpms = 1:25;
-corrs = 1:18;
+bpms = 1:50;
+corrs = 1:42;
 
 nbpm=length(bpms);
 ncorr=length(corrs);
-nxbpm=2;
+nxbpm=3;
 nxcorr=0;
-nk=1;
+nk=0;
+bpmungain = true;
+corrungain = true;
 
 M_ = M(bpms,corrs);
 
@@ -30,33 +32,52 @@ ye=[];
 uv=[];
 yv=[];
 
+nexp=1;
 i=1;
 for corr = corrs
-    u = bpm_iddata_id{1}.InputData(:,corr);
+    if nexp == 1
+        u = bpm_iddata_id{nexp}.InputData(:,corr);
+    else
+        if corr == nexp-1
+            u = bpm_iddata_id{nexp}.InputData(:,1);
+        else
+            u = zeros(prbsperiod,1);
+        end
+    end
     um = lsim(syscorrarray{corr}, [u; u]);
     ue(:,i) = um(prbsperiod+1:2*prbsperiod);
     uv(:,i) = um(prbsperiod+1:2*prbsperiod);
     i=i+1;
 end
 
-%param = 
+datae = iddata(bpm_iddata_id{nexp}.OutputData(:,bpms),ue,Ts);
+datav = iddata(bpm_iddata_val{nexp}.OutputData(:,bpms),uv,Ts);
 
-param = zeros(nbpm*(2*nxbpm+1) + ncorr*(2*nxcorr+1),1);
-%load parameters; param = repmat(param, 1, 2);
+param = [];
+%param = [param; zeros(nbpm*(2*nxbpm),1); ones(nbpm,1)];
+param = [param; ...
+    repmat([0.153478424046198; 0.492048732715991; -0.361653828353039], nbpm, 1); ...
+    repmat([0.318634552964485; 0.279272511234536; 0.0354099666806719], nbpm, 1)];
 
-param = [repmat([-0.0399 0.5288], 1, nbpm)  repmat([0.6777 0.1918], 1, nbpm) repmat(1, 1, nbpm)];
+if ~bpmungain
+    param = [param; repmat(0.0326171204852166, nbpm, 1)];
+end
 
-fofbmodel = idgrey('idfofbmodel',param,'d',[nk,nbpm,ncorr,nxbpm,nxcorr,M_(:)'],'InitialState','Zero');
+param = [param; zeros(ncorr*(2*nxcorr),1)];
+if ~corrungain
+    param = [param; ones(ncorr,1)];
+end
+
+auxvars = [nbpm,ncorr,nxbpm,nxcorr,bpmungain,corrungain,M_(:)'];
+fofbmodel = idgrey('idfofbmodel',param,'d', auxvars,'InitialState','Zero');
+fofbmodel.InputDelay = repmat(nk, ncorr, 1);
 fofbmodel.Ts = Ts;
 fofbmodel.Algorithm.Focus = 'Simulation';
 fofbmodel.Algorithm.Display = 'On';
 
-datae = iddata(bpm_iddata_id{1}.OutputData(:,bpms),ue,Ts);
-datav = iddata(bpm_iddata_id{1}.OutputData(:,bpms),uv,Ts);
-
 datae.Ts = Ts;
 m4 = pem(detrend(datae,0), fofbmodel);
 
-[~,fit] = compare(m4,detrend(datav,0));
+[~,fit] = compare(m4,detrend(datav,0), Inf);
 fit = squeeze(fit);
 hold all; plot(fit)
