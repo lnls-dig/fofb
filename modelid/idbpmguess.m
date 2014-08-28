d92e62b2d17f612bf0340f73b5f7f188fdeae2bc
Daniel Tavares <daniel.tavares@lnls.br>
2014-08-28 17:11:19 -0300

    modelid/idbpmguess.m, modelid/idpreprocessdata.m: several modifications to give more flexibility to identification

--- modelid/idbpmguess.m
+++ modelid/idbpmguess.m
@@ -4,47 +4,97 @@
 
 prbsperiod = 2047;
 
-order_corr = [4 3 0];
+if false
+    load(fullfile('~', 'dados_ident','master','sysident_data_2014.07.27','processed','beam energy = 1.37 GeV, IDs = closed','AON11 gap = 22 mm, SCW = 3.5 T, user condition (correct tune, coupling, etc.)','step','2014.07.27_17.23.42.mat'))
+    M=idrespm(bpm_iddata, 6000);
+    
+    load(fullfile('~', 'dados_ident','master','sysident_data_2014.07.27','processed','beam energy = 1.37 GeV, noise','2014.07.27.mat'));
+    
+    load(fullfile('~', 'dados_ident','master','sysident_data_2014.07.27','processed','beam energy = 1.37 GeV, IDs = closed','AON11 gap = 22 mm, SCW = 3.5 T, user condition (correct tune, coupling, etc.)','prbs','2014.07.27_17.28.30.mat'))
+    [bpm_iddata_id, bpm_iddata_val] = idprbsmean(bpm_iddata, prbsperiod, [], false);
+    [corr_iddata_id, corr_iddata_val] = idprbsmean(corr_iddata, prbsperiod, [], false);
+end
+
+%idmethod_corrps = @n4sid;
+idmethod_corrps = @arx;
+order_corrps = [4 4 0];
+
+% idmethod_bpm = @n4sid;
+idmethod_bpm = @arx;
 order_bpm = [2 2 1];
 
-bpmhvsymmetry = 0;
 bpmi = 1:50;
 corri = 1:42;
 
 bpmungain = 1;
+corrpsungain = 0;
+bpmhvsymmetry = 0;
 
+cicfilter_netdelay = tf([0.18 0.64 0.18], [1 0 0],  -1, 'InputDelay', 1);
+
+Ts = bpm_iddata_id{1}.Ts;
+
+
+nbpm = length(bpmi);
+ncorr = length(corri);
+
+nnoise = 100;
+
+% for i=1:ncorr
+%     tic
+%     m = ar(corr_iddata_noise.OutputData(:,i), nnoise);
+%     syscorrnoise{i} = m.a;
+%     toc
+% end
+% 
+% for i=1:nbpm
+%     tic
+%     m = ar(bpm_iddata_noise.OutputData(:,i), nnoise);
+%     sysbpmnoise{i} = m.a;
+%     toc
+% end
+
+
+% Black box identification of orbit corrector power supplies models
 datae = {};
 datav = {};
-for i=1:length(corri)
-    datae{i} = corr_iddata_id{corri(i)+1};
-    datav{i} = corr_iddata_val{corri(i)+1};
+for i=1:ncorr
+    datae{i} = idfilt(detrend(corr_iddata_id{corri(i)+1},0), {syscorrnoise{i},[1 zeros(1,length(syscorrnoise{i}))]}, 'causal');
+    datav{i} = idfilt(detrend(corr_iddata_val{corri(i)+1},0), {syscorrnoise{i},[1 zeros(1,length(syscorrnoise{i}))]}, 'causal');
 end
 
-fixedsys = tf([0.18 0.64 0.18], [1 0 0],  -1, 'InputDelay', 1);
-[acorr, bcorr, fitcorr] = idsiso(datae, datav, order_corr, 1, fixedsys);
+[acorrps, bcorrps, fitcorrps, rcorrps] = idsiso(datae, datav, idmethod_corrps, order_corrps, 1, cicfilter_netdelay);
+syscorrps = idab2sys(acorrps, bcorrps, Ts);
 
-syscorrps = idab2sys(acorr,bcorr);
-
-% --
-
-bpmi = bpmi(:);
-
+% Black box identification of BPM electronics + beam dynamics + vacuum chamber + orbit corrector magnetic core model
+% Use experiments where only one orbit corrector was excited. For each BPM, take the corrector that produces the largest distortion at the location of that BPM.
 M_ = M(bpmi, corri);
 [~, corribpmi] = max(abs(M_), [], 2);
 gains = M_(sub2ind(size(M_),(1:size(M_,1))',corribpmi));
 
 datae = {};
 datav = {};
-for i=1:length(bpmi)
-    datae{i} = bpm_iddata_id{corri(corribpmi(i))+1}(:,bpmi(i));
-    datav{i} = bpm_iddata_val{corri(corribpmi(i))+1}(:,bpmi(i));
+for i=1:nbpm
+    datae{i} = idfilt(detrend(bpm_iddata_id{bpmi(i)+1},0), {sysbpmnoise{i},[1 zeros(1,length(sysbpmnoise{i}))]}, 'causal');
+    datav{i} = idfilt(detrend(bpm_iddata_val{bpmi(i)+1},0), {sysbpmnoise{i},[1 zeros(1,length(sysbpmnoise{i}))]}, 'causal');
     
     datae{i}.OutputData = datae{i}.OutputData/gains(i);
     datav{i}.OutputData = datav{i}.OutputData/gains(i);
 end
-[abpm, bbpm, fitbpm] = idsiso(datae, datav, order_bpm, syscorrps(corribpmi), fixedsys);
 
-sysbpm = idab2sys(abpm,bbpm);
+[abpm, bbpm, fitbpm, rbpm] = idsiso(datae, datav, idmethod_bpm, order_bpm, syscorrps(corribpmi), cicfilter_netdelay);
+sysbpm = idab2sys(abpm, bbpm, Ts);
+
+% Grey box identification of the complete FOFB model using individual orbit corrector, BPM and static beam response matrix
+% as starting point and static beam response matrix obtained from step experiments
+[datae, datav] = idpreprocessdata(bpm_iddata_id{1}, bpm_iddata_val{1}, corri, bpmi, 1, cicfilter_netdelay);
+
+% Scaling factor applied to input-output data in order to avoid reaching floating point resolution limits on cost function calculation 
+f = 1e2;
+datae.InputData = datae.InputData*f;
+datae.OutputData = datae.OutputData*f;
+datav.InputData = datav.InputData*f;
+datav.OutputData = datav.OutputData*f;
 
 if bpmhvsymmetry
     % Take only vertical BPM responses
@@ -52,18 +102,19 @@ if bpmhvsymmetry
     bbpm = bbpm(end/2+1:end, :);
 end
 
-% --
+num = num2cell(zeros(ncorr));
+den = num2cell(ones(ncorr));
+for i=1:ncorr, num{i,i} = bcorrps{i}; den{i,i} = acorrps{i}; end
+syscorrpsmimo = tf(num,den,320e-6);
 
-[datae, datav] = idpreprocessdata(bpm_iddata_id{1}, bpm_iddata_val{1}, corri, bpmi, 1, fixedsys, prbsperiod);
+num = num2cell(zeros(nbpm));
+den = num2cell(ones(nbpm));
+for i=1:nbpm, num{i,i} = bbpm{i}; den{i,i} = abpm{i}; end
+sysbpmmimo = tf(num,den,320e-6);
 
-datae.InputData = datae.InputData*1e4;
-datae.OutputData = datae.OutputData*1e4;
-datav.InputData = datav.InputData*1e4;
-datav.OutputData = datav.OutputData*1e4;
+[param, fitfofb] = idgbbpm(datae, datav, order_bpm, order_corrps, abpm, bbpm, acorrps, bcorrps, M_, bpmungain, corrpsungain, bpmhvsymmetry);
 
-[param, fitfofb] = idgbbpm(datae, datav, order_bpm, order_corr, abpm, bbpm, acorr, bcorr, M_, bpmungain, bpmhvsymmetry);
-
-
+% Presentation of results
 pbpmoffset = 0;
 if ~bpmungain
     pcorroffset = size(abpm,1)*(sum(order_bpm(1:2)));
@@ -71,8 +122,11 @@ else
     pcorroffset = size(abpm,1)*(sum(order_bpm(1:2))-1);
 end
 
-parambpm = param(pbpmoffset+1:pcorroffset);
-paramcorr = param(pcorroffset+1:end);
+param_bpm = param(pbpmoffset+1:pcorroffset);
+param_corrps = param(pcorroffset+1:end);
 
-[abpm_idgb,bbpm_idgb] = idparam2ab(parambpm, size(abpm,1), order_bpm, bpmungain);
-[acorr_idgb,bcorr_idgb] = idparam2ab(paramcorr, size(acorr,1), order_corr);
+[abpm_idgb,bbpm_idgb] = idparam2ab(param_bpm, size(abpm,1), order_bpm, bpmungain);
+[acorr_idgb,bcorr_idgb] = idparam2ab(param_corrps, size(acorrps,1), order_corrps, corrpsungain);
+
+sysbpm2 = idab2sys(abpm_idgb, bbpm_idgb, Ts);
+syscorrps2 = idab2sys(acorr_idgb, bcorr_idgb, Ts);
