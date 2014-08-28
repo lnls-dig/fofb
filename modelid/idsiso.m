4f531ebb7ff909b69849c962b33ad52c9569577a
Daniel Tavares <daniel.tavares@lnls.br>
2014-08-28 17:10:23 -0300

    modelid/idsiso.m: special options for non-polynomial models; coefficients are now stored in cell arrays, not matrices

--- modelid/idsiso.m
+++ modelid/idsiso.m
@@ -1,25 +1,48 @@
-function [a, b, fit] = idsiso(datae, datav, order, sysarray, fixedsys)
+function [a, b, fit, rcorr] = idsiso(datae, datav, idmethod, order, sysarray, fixedsys)
 
-na = order(1);
-nb = order(2);
-nk = order(3);
-
+nlags = 25;
 nu = length(datae);
-a = zeros(nu, max(na+1,nb+nk));
-b = zeros(nu, max(na+1,nb+nk));
+
+% na = order(1);
+% nb = order(2);
+% nk = order(3);
+% a = zeros(nu, max(na+1,nb+nk));
+% b = zeros(nu, max(na+1,nb+nk));
+
+a = {};
+b = {};
+
 fit = zeros(nu,1);
+rcorr = zeros(2*nlags+1, nu);
 for i = 1:nu
     if iscell(sysarray)
         sys = sysarray{i};
     else
         sys = sysarray;
     end
-    [datae_, datav_] = idpreprocessdata(detrend(datae{i},0), detrend(datav{i},0), 1, 1, sys, fixedsys, size(datae{i},1));
-    m = arx(datae_, order, 'Focus', 'Simulation');
+    [datae_, datav_] = idpreprocessdata(detrend(datae{i},0), detrend(datav{i},0), 1, 1, sys, fixedsys);
+    
+    if isequal(idmethod, @impulse)
+        m = idmethod(datae_, 'PW', order(1));
+    elseif  isequal(idmethod, @n4sid)
+        %nk = delayest(datae_);
+        %nk = max(nk, 1);
+        m = idmethod(datae_, 'best');%, 'nk', nk);
+        %m = pem(m,datae_);
+        %plot(fft(datae_)); pause
+    else
+        m = idmethod(datae_, order, 'Focus', 'Simulation');
+    end
+        
     [~, fit(i)] = compare(m, datav_, Inf);
+    
+    r = resid(m, datav_);
+    rcorr(:,i) = xcorr(r.OutputData, r.InputData, nlags, 'coeff');
+    
     mtf = tf(m, 'measured');
     den = mtf.den{1};
     num = mtf.num{1};
-    a(i,:) = den;
-    b(i,:) = num;
+    
+    a{i} = den;
+    b{i} = num;
 end
