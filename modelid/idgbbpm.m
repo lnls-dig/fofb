47c9c9e4e781c3f33f64866f4b33865e92ac6d4f
Daniel Tavares <daniel.tavares@lnls.br>
2014-08-28 17:06:56 -0300

    modelid/idgbbpm.m: option for keeping power supply gains unitary

--- modelid/idgbbpm.m
+++ modelid/idgbbpm.m
@@ -1,10 +1,10 @@
-function [param, fit] = idgbbpm(datae, datav, order_bpm, order_corr, abpm, bbpm, acorr, bcorr, M, bpmungain, bpmhvsymmetry)
+function [param, fit] = idgbbpm(datae, datav, order_bpm, order_corr, abpm, bbpm, acorr, bcorr, M, bpmungain, corrungain, bpmhvsymmetry)
 
 [~, nbpm, ncorr] = size(datae);
 
-param = [idab2param(abpm, bbpm, order_bpm, bpmungain) idab2param(acorr, bcorr, order_corr)];
+param = [idab2param(abpm, bbpm, order_bpm, bpmungain) idab2param(acorr, bcorr, order_corr, corrungain)];
 
-auxvars = [nbpm, ncorr, order_bpm, order_corr, [0 0], bpmungain, 0, bpmhvsymmetry, M(:)'];
+auxvars = [nbpm, ncorr, order_bpm, order_corr, [0 0], bpmungain, corrungain, bpmhvsymmetry, M(:)'];
 fofbmodel = idgrey('idfofbmodel', param, 'd', auxvars, 'InitialState', 'Zero', 'OutputName', datav.OutputName, 'InputName', datav.InputName);
 fofbmodel.Ts = datae.Ts;
 fofbmodel.Algorithm.Focus = 'Simulation';
