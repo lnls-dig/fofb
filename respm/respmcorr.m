af7f2270d31d37be39a99e6785aa35dbb4de9cfa
Daniel Tavares <daniel.tavares@lnls.br>
2018-10-17 10:05:12 -0300

    respmcorr.m: remove unused lines

--- respm/respmcorr.m
+++ respm/respmcorr.m
@@ -11,10 +11,8 @@ M = zeros(n_orbit_points, nelements, 4);
 
 original_kick = lnls_get_kickangle(THERING, indexes, plane);
 for i=1:nelements
-    kicks = original_kick;
     THERING = lnls_set_kickangle(THERING, original_kick(i)-delta_exc/2, indexes(i), plane);
     orbit1 = findorbit4(THERING, 0, orbit_points);
-    kicks(i) = kicks(i)-delta_exc/2;
     THERING = lnls_set_kickangle(THERING, original_kick(i)+delta_exc/2, indexes(i), plane);
     orbit2 = findorbit4(THERING, 0, orbit_points);
     M(:,i,:)  = (orbit2 - orbit1)'/delta_exc;
