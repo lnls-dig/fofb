ee1feafb2f3657688a4d47856b9545f22fe75e70
Daniel Tavares <daniel.tavares@lnls.br>
2019-11-16 23:27:27 -0300

    respmcorr.m: use new fields CH and CV to excite correctors inside sextupoles

--- respm/respmcorr.m
+++ respm/respmcorr.m
@@ -9,15 +9,27 @@ M = zeros(n_orbit_points, nelements, 4);
 for i=1:nelements
     if strcmpi(plane{i}, 'h')
         planenum = 1;
+        planecorr = 'CH';
     else
         planenum = 2;
+        planecorr = 'CV';
     end
-    original_kick = THERING{indexes(i,:)}.KickAngle(planenum);
-    THERING{indexes(i,:)}.KickAngle(planenum) = original_kick - (delta_exc/2);
-    orbit1 = findorbit4(THERING, 0, orbit_points);
-    THERING{indexes(i,:)}.KickAngle(planenum) = original_kick + (delta_exc/2);
-    orbit2 = findorbit4(THERING, 0, orbit_points);
-    THERING{indexes(i,:)}.KickAngle(planenum) = original_kick;
+    
+    if isfield(THERING{indexes(i,:)}, 'KickAngle')
+        original_kick = THERING{indexes(i,:)}.KickAngle(planenum);
+        THERING{indexes(i,:)}.KickAngle(planenum) = original_kick - (delta_exc/2);
+        orbit1 = findorbit4(THERING, 0, orbit_points);
+        THERING{indexes(i,:)}.KickAngle(planenum) = original_kick + (delta_exc/2);
+        orbit2 = findorbit4(THERING, 0, orbit_points);
+        THERING{indexes(i,:)}.KickAngle(planenum) = original_kick;
+    elseif isfield(THERING{indexes(i,:)}, 'CH') || isfield(THERING{indexes(i,:)}, 'CV')
+        original_kick = THERING{indexes(i,:)}.(planecorr);
+        THERING{indexes(i,:)}.(planecorr) = original_kick - (delta_exc/2);
+        orbit1 = findorbit4(THERING, 0, orbit_points);
+        THERING{indexes(i,:)}.(planecorr) = original_kick + (delta_exc/2);
+        orbit2 = findorbit4(THERING, 0, orbit_points);
+        THERING{indexes(i,:)}.(planecorr) = original_kick;
+    end
 
     M(:,i,:)  = (orbit2 - orbit1)'/delta_exc;
 end
