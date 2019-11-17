e79ca406a945d26a3555ebe825f82efc7b9d9ad0
Daniel Tavares <daniel.tavares@lnls.br>
2019-11-16 23:22:27 -0300

    respmsirius.m: change quadrupole and correctors index retrieval to macth new machine version
    After many changes in Sirius lattice description, the Family names
    of quadrupoles have changed and the slow correctors were placed
    inside sextupoles themselves (not a virtual corrector inside sextupoles).
    
    The old scheme has worked up to the version SI.V06.

--- respm/respmsirius.m
+++ respm/respmsirius.m
@@ -48,15 +48,44 @@ mia  = findcells(THERING, 'FamName', 'mia')';
 mib  = findcells(THERING, 'FamName', 'mib')';
 
 % Quadrupoles
-qad = findcells(THERING, 'FamName', 'qad')';
-qaf  = findcells(THERING, 'FamName', 'qaf')';
-qbf  = findcells(THERING, 'FamName', 'qbf')';
-qf1  = findcells(THERING, 'FamName', 'qf1')';
-qf2  = findcells(THERING, 'FamName', 'qf2')';
-qf3  = findcells(THERING, 'FamName', 'qf3')';
-qf4  = findcells(THERING, 'FamName', 'qf4')';
-qbd1  = findcells(THERING, 'FamName', 'qbd1')';
-qbd2 = findcells(THERING, 'FamName', 'qbd2')';
+QDA  = findcells(THERING, 'FamName', 'QDA')';
+QFA  = findcells(THERING, 'FamName', 'QFA')';
+QFB  = findcells(THERING, 'FamName', 'QFB')';
+Q1   = findcells(THERING, 'FamName', 'Q1')';
+Q2   = findcells(THERING, 'FamName', 'Q2')';
+Q3   = findcells(THERING, 'FamName', 'Q3')';
+Q4   = findcells(THERING, 'FamName', 'Q4')';
+QDB1 = findcells(THERING, 'FamName', 'QDB1')';
+QDB2 = findcells(THERING, 'FamName', 'QDB2')';
+
+% Sextupoles with correctors CH and CV
+SDA0 = findcells(THERING, 'FamName', 'SDA0')';
+SDA1 = findcells(THERING, 'FamName', 'SDA1')';
+SDB1 = findcells(THERING, 'FamName', 'SDB1')';
+SFB0 = findcells(THERING, 'FamName', 'SFB0')';
+SFP0 = findcells(THERING, 'FamName', 'SFP0')';
+SDP1 = findcells(THERING, 'FamName', 'SDP1')';
+SFP2 = findcells(THERING, 'FamName', 'SFP2')';
+
+% Sextupoles with correctors CH
+SFA2 = findcells(THERING, 'FamName', 'SFA2')';
+
+% Sextupoles with correctors CV
+SDA3 = findcells(THERING, 'FamName', 'SDA3')';
+SDB3 = findcells(THERING, 'FamName', 'SDB3')';
+SDP3 = findcells(THERING, 'FamName', 'SDP3')';
+
+% Sextupoles with CH or CV depending on sector
+SFB2 = findcells(THERING, 'FamName', 'SFB2')';
+SFB2_CH = SFB2;
+SFB2_CV = SFB2(1:2:end);
+
+% Dedicated vertical correctors
+CV = findcells(THERING, 'FamName', 'CV')';
+
+% Dedicated vertical correctors
+FC1 = findcells(THERING, 'FamName', 'FC1')';
+FC2 = findcells(THERING, 'FamName', 'FC2')';
 
 % Dipoles
 bc = findcells(THERING, 'FamName', 'bc')';
@@ -73,10 +102,10 @@ b3 = reshape(b3, nsegs_b3, []);
 bpm = findcells(THERING, 'FamName', 'BPM')';
 id = sort([mia; mib]);
 source =  sort([mc; id]);
-hcm = findcells(THERING, 'FamName', 'hcm')';
-vcm = findcells(THERING, 'FamName', 'vcm')';
-crhv = findcells(THERING, 'FamName', 'crhv')';
-quad = sort([qad; qaf; qbd1; qbd2; qbf; qf1; qf2; qf3; qf4]);
+hcm = sort([SDA0; SDA1; SDB1; SFB0; SFP0; SDP1; SFP2; SFA2; SFB2_CH]);
+vcm = sort([SDA0; SDA1; SDB1; SFB0; SFP0; SDP1; SFP2; SDA3; SDB3; SDP3; SFB2_CV; CV]);
+crhv = sort([FC1; FC2]);
+quad = sort([QDA; QFA; QDB1; QDB2; QFB; Q1; Q2; Q3; Q4]);
 dipole = sort([bc b1 b2 b3])';
 rf = findcells(THERING, 'FamName', 'cav')';
 
