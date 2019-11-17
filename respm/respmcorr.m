ce6c3ee01cf9485c13fd214835f85e762544b0ca
Daniel Tavares <daniel.tavares@lnls.br>
2019-11-17 19:19:06 -0300

    respmcorr.m: remove obsolete selection of variables per plane

--- respm/respmcorr.m
+++ respm/respmcorr.m
@@ -7,14 +7,6 @@ n_orbit_points = length(orbit_points);
 nelements = size(indexes,1);
 M = zeros(n_orbit_points, nelements, 4);
 for i=1:nelements
-    if strcmpi(plane{i}, 'h')
-        planenum = 1;
-        planecorr = 'CH';
-    else
-        planenum = 2;
-        planecorr = 'CV';
-    end
-    
     original_kick = lnls_get_kickangle(THERING, indexes, plane);
     THERING = lnls_set_kickangle(THERING, original_kick(i)-delta_exc/2, indexes(i), plane);
     orbit1 = findorbit4(THERING, 0, orbit_points);
