0b9a3b92bd6e91a3caa5705d02d46bb9c949c403
Daniel Tavares <daniel.tavares@lnls.br>
2020-01-03 17:35:05 -0300

    respmtheor: matrix calculation optimization
    1) Lower memory utilization on matrix calculation
    2) Do not use diagonal matrices to implement betatron modulation - use loops instead

--- respm/respmtheor.m
+++ respm/respmtheor.m
@@ -17,12 +17,23 @@ eta_orbit = repmat(eta(orbit_indexes), 1, length(corr_indexes));
 eta_corr = repmat(eta(corr_indexes)', length(orbit_indexes), 1);
 dispersive_orbit = eta_orbit.*eta_corr/alpha/L;
 
+% Build response matrix
 % Betatron "modulation"
-ph = pi*tune - abs(ph_orbit_minus_corr);
-sqrt_beta_bpm = diag(sqrt(beta(orbit_indexes)));
-sqrt_beta_cm = diag(sqrt(beta(corr_indexes)));
+M = cos(pi*tune - abs(ph_orbit_minus_corr));
 
-% Gain
-gain = 1/2/sin(pi*tune);
+% Tune term
+M = M/2/sin(pi*tune);
 
-M = gain*sqrt_beta_bpm*cos(ph)*sqrt_beta_cm + dispersive_orbit;
+% Betatron "modulation" term
+sqrt_beta_orbit = sqrt(beta(orbit_indexes));
+sqrt_beta_cm = sqrt(beta(corr_indexes));
+
+for i=1:length(orbit_indexes)
+    M(i,:) = M(i,:).*sqrt_beta_cm';
+end
+for j=1:length(corr_indexes)
+    M(:,j) = M(:,j).*sqrt_beta_orbit;
+end
+
+% Dispersive orbit term
+M = M + dispersive_orbit;
