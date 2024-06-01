fe2e73edac6725395c742e4aa00391ed303ff94f
Daniel Tavares <daniel.tavares@lnls.br>
2024-06-01 16:30:02 -0300

    ofbmdl.m: add loop shaping filters to the controller transfer function

--- ofbmdl.m
+++ ofbmdl.m
@@ -1,9 +1,11 @@
-function T = ofbmdl(M, Mc, K, corr_sys, F, Wd, Wz)
-
-%TODO: add shaping filters - F(z)
+function [T, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz)
 
 nbpms = size(M,1);
 ncorr = size(M,2);
+
+if nargin < 5 || isempty(F)
+    F = tf(1, 1, -1);
+end
 
 if nargin < 6 || isempty(Wd)
     Wd = eye(nbpms);
@@ -13,34 +15,27 @@ if nargin < 7 || isempty(Wz)
     Wz = eye(nbpms);
 end
 
-% Actuator transfer function (includes power supply, magent, vacuum
+% Actuator trasnfer function (includes power supply, magent, vacuum
 % chamber, BPM, beam and network delay dynamics)
-ncorr_sys = length(corr_sys);
-corr_num = cell(ncorr);
-corr_num(:,:) = {0};
-corr_den = cell(ncorr);
-corr_den(:,:) = {1};
-for i=1:ncorr_sys
-    corr_tf_i = tf(corr_sys{i});
-    corr_tf_i = absorbDelay(corr_tf_i);
-    corr_num{i,i} = corr_tf_i.num{1};
-    corr_den{i,i} = corr_tf_i.den{1};
-    %corr_delay(i,i) = totaldelay(corr_tf_i);
+[A, Ts] = sys_array2matrix(A, ncorr);
+
+% Loop shaping filters
+[F, Ts_F] = sys_array2matrix(F, ncorr);
+
+if Ts_F ~= -1 && Ts ~= Ts_F
+    error('Sample time ''Ts'' of actuator transfer functions and loop shaping filters must be equal.');
 end
-Ts = corr_tf_i.Ts;
-%corr_tf = tf(corr_num, corr_den, Ts, 'ioDelay', corr_delay);
-corr_tf = tf(corr_num, corr_den, Ts);
 
 % Plant transfer function
-G = ss(M)*ss(corr_tf);
+G = ss(M)*ss(A);
 
 % Controller transfer function
-C = diag(K)*Mc*tf([Ts 0], [1 -1], Ts);
+C = ss(F)*diag(K)*Mc*tf([Ts 0], [1 -1], Ts);
 
-% Orbit disturbance transfer function
+% Orbit disturbance weighting matrix
 Wd = ss(Wd);
 
-% Performance transfer function
+% Performance weighting matrix
 Wz = ss(Wz);
 
 % Port names
@@ -58,3 +53,39 @@ sum_yd = sumblk('yd = y + dd', nbpms);
 sum_e = sumblk('e = r - yd', nbpms);
 
 T = connect(G, C, Wd, Wz, sum_yd, sum_e, {'r','d'}, {'yd','z'}, {'u'});
+
+function [sys_matrix, Ts] = sys_array2matrix(sys_array, nsys)
+
+if ~iscell(sys_array)
+    sys_array = {sys_array};
+end
+
+if length(sys_array) == 1
+    corr_sys_ = cell(1, nsys);
+    corr_sys_(:) = sys_array;
+    sys_array = corr_sys_;
+end
+
+if length(sys_array) ~= nsys
+    error('Number of elements of ''corr_sys'' must equal ''ncorr''.');
+end
+
+corr_num = cell(nsys);
+corr_num(:,:) = {0};
+corr_den = cell(nsys);
+corr_den(:,:) = {1};
+Ts_array = zeros(nsys, 1);
+for i=1:nsys
+    corr_tf_i = tf(sys_array{i});
+    corr_tf_i = absorbDelay(corr_tf_i);
+    corr_num{i,i} = corr_tf_i.num{1};
+    corr_den{i,i} = corr_tf_i.den{1};
+    Ts_array(i) = corr_tf_i.Ts;
+end
+Ts = Ts_array(1);
+
+if ~all(Ts_array == Ts)
+    error('Sample time ''Ts'' of all ''corr_sys'' systems must be identical.');
+end
+
+sys_matrix = tf(corr_num, corr_den, Ts);
