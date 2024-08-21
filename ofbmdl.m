fdbd08ecec6a3fb943b6582abed71e01c1a3cb8c
Lucas Antonio Pelike <lucas.pelike@lnls.br>
2024-08-21 15:04:32 -0300

    Added fit constants

--- ofbmdl.m
+++ ofbmdl.m
@@ -1,4 +1,4 @@
-function [P, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz, Wn)
+function [P, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz, Wn, eta)
 % OFBMDL Orbit Feedback Model.
 % 
 % Construct orbit feedback state-space model.
@@ -14,7 +14,7 @@ function [P, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz, Wn)
 %         cell array of dynamic system objects with NCORR elements)
 %         encompassing power supply, magnet, vacuum chamber, BPM and
 %         network delay dynamics. If only one system is provided all are
-%         assumed to have identical responses.
+%         assumed to have identical responses. 
 %   F:    (Optional input) Shaping filters per orbit corrector (dynamic
 %         system object (tf, zpk, ss, etc.) or cell array of dynamic system
 %         objects with NCORR elements). If only one filter is provided the
@@ -25,6 +25,7 @@ function [P, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz, Wn)
 %         optimzation purposes.
 %   Wn:   (Optional input) Noise weighting matrix, used for loop
 %         optimzation purposes.
+%   eta:  (Optional input) RF column in the orbit response matrix. Dimensions NBPMx1.
 %
 % OUTPUTS:
 %   P:    Generalized plant of the closed-loop orbit feedback system.
@@ -34,6 +35,7 @@ function [P, G, C] = ofbmdl(M, Mc, K, A, F, Wd, Wz, Wn)
 
 nbpms = size(M,1);
 ncorr = size(M,2);
+rf_interaction=true;
 
 if nargin < 5 || isempty(F)
     F = tf(1, 1, -1);
@@ -51,6 +53,10 @@ if nargin < 8 || isempty(Wn)
     Wn = eye(nbpms);
 end
 
+if nargin < 9 || isempty(eta)
+    rf_interaction=false;
+end
+
 % Actuator transfer function (includes power supply, magnet, vacuum
 % chamber, BPM, beam and network delay dynamics)
 [A, Ts] = sys_array2matrix(A, ncorr);
@@ -62,11 +68,35 @@ if Ts_F ~= -1 && Ts ~= Ts_F
     error('Sample time ''Ts'' of actuator transfer functions and shaping filters must be equal.');
 end
 
-% Plant transfer function
-G = ss(M)*ss(A);
-
-% Controller transfer function
-C = ss(F)*diag(K)*Mc*tf([Ts 0], [1 -1], Ts);
+%Adds optional RF interaction
+if rf_interaction
+    %RF parameters obtained from fit_rf.m and wiki-sirius
+    %https://wiki-sirius.lnls.br/mediawiki/index.php/Machine:RF_System
+    sync_freq = 2.1515e3; %Hz (Fit)
+    alpha_s = 1.2547e3; %(Fit)
+    rf_freq = 499.664e6; %Hz (Theoretical)
+    alpha_c = 1.6e-4; %(Theoretical)
+    %RF phase to beam transfer function
+    %https://journals.aps.org/prab/pdf/10.1103/PhysRevAccelBeams.25.082801
+    H = tf([sync_freq^2/rf_freq/alpha_c 0],[1 2*alpha_s sync_freq^2]);
+    H = c2d(H, Ts);%*1e9; %adjusting for nm
+    H = H.*eta;
+    % New M and altered Mc
+    Mrf = [M H];
+    Mc = pinv([M zeros(length(eta),1)]);
+    Arf = [A; ones(ncorr,1)'];
+    % Altered shaping filters
+    F(ncorr+1,ncorr+1)=tf(1, 1, Ts);
+    % Plant transfer function
+    G = ss(Mrf)*ss(Arf);
+    % Controller transfer function
+    C = ss(F)*diag(K)*Mc*tf([Ts 0], [1 -1], Ts);
+else
+    % Plant transfer function
+    G = ss(M)*ss(A);
+    % Controller transfer function
+    C = ss(F)*diag(K)*Mc*tf([Ts 0], [1 -1], Ts);
+end
 
 % Orbit disturbance weighting matrix
 Wd = ss(Wd);
