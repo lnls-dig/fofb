fc99fc0444bd7780fa23a192780f6f212762fc5e
Daniel Tavares <daniel.tavares@lnls.br>
2025-08-01 13:56:42 -0300

    bwlpred.m: improve interface (input parameters definitions)

--- bwlpred.m
+++ bwlpred.m
@@ -1,18 +1,22 @@
-function p = bwlpred(a, dly, Ts)
+function p = bwlpred(dly, Amax, Ts)
 % BWLPRED Band-limited discrete-time predictor.
 %
-% p = BWLPRED(a, dly, Ts)
+% p = BWLPRED(dly, Amax, Ts)
 %
 % INPUTS:
-%   a:   Parameter to tune predictor bandwidth.
-%   dly: Delay (in samples unit) to be predicted (multiple of 0.5).
-%   Ts:  (Optional input) Sample time. If not specified -1 is assumed.
+%   dly:    Delay (in samples unit) to be predicted (multiple of 0.5).
+%   Amax:   (Optional) Maximum amplification (or gain at 1/Ts/2). Default
+%           value is 10^dly.
+%   Ts:     (Optional) Sample time. If not specified -1 is assumed.
 %
 % OUTPUTS:
 %   p:   Transfer function object of the band-limited predictor.
 
-if a < 1
-    error('''a'' must be equal or greater than 1 to result in a stable predictor.');
+if nargin < 2 || isempty(Amax)
+    Amax = 10^dly;
+end
+if Amax <= 1
+    error('''Amax'' must be greater than 1 to result in a stable predictor.');
 end
 
 N = 2*dly;
@@ -24,4 +28,6 @@ if nargin < 3 || isempty(Ts)
     Ts = -1;
 end
 
-p = tf([1+a 0], [a 1], Ts)^N;
+b = Amax^(1/N);
+c = (b+1)/(b-1);
+p = tf([1+c 0], [c 1], Ts)^N;
